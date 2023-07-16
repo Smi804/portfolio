b9e84a5360ed7d60abcb0f46c04df3223c755ad4
smi804 <abbasdanish804@gmail.com>
2023-07-16 14:07:53 +0500

    first

---
+++ practice-react/src/components/AboutContent.js
@@ -0,0 +1,33 @@
+import "./AboutContentStyles.css"
+import React from 'react'
+import { Link } from "react-router-dom";
+import react1 from "../assets/react1.jpg";
+import react2 from "../assets/react2.jpg";
+
+
+function AboutContent() {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p> I am a react front end developer. 
+                i create responsive secure websites for my clients. </p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={react1} className="img" alt="true"/>
+                </div>
+                <div className="img-stack bottom">
+                    <img src={react2} className="img" alt="true"/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent
---
+++ practice-react/src/components/WorkCardData.js
@@ -0,0 +1,25 @@
+import pro1 from "../assets/project1.jpg"
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/project3.jpg"
+
+const ProjectCardData=[
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Website Design",
+        text:"Optionally you can add the ability to give a score to the user at the end of the game, if the score is above a threshold value you declare the player to be a winner, using some gifs corresponding to winning and losing the game.",
+        view:"https://react.dev/"
+    },{
+            imgsrc:pro2,
+            title:"LMS Website Design",
+            text:"Optionally you can add the ability to give a score to the user at the end of the game, if the score is above a threshold value you declare the player to be a winner, using some gifs corresponding to winning and losing the game.",
+            view:"https://react.dev/"
+        },
+        {
+            imgsrc:pro3,
+            title:"Netflix Website Design",
+            text:"Optionally you can add the ability to give a score to the user at the end of the game, if the score is above a threshold value you declare the player to be a winner, using some gifs corresponding to winning and losing the game.",
+            view:"https://react.dev/"
+        }
+    ]
+
+    export default ProjectCardData;
